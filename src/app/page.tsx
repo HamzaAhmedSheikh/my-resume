ee4b6ced960d810b61d3b2a40388c6168f3ef74f
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-03-11 04:54:15 +0500

    I have made a navbar for the resume website.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "./page.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
-  return (
-    <>
-      <h1> hello </h1>
-    </>
-  );
+  return <>{/* <Navbar /> */}</>;
 }
 
