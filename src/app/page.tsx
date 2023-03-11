1cc7c4ec868c50952be873f408d382c8c6a61b5c
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-03-11 10:54:24 +0500

    I have made the hero section for the resume website.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
-import Navbar from "@/components/Navbar";
+import Main from "@/components/Main";
 
 export default function Home() {
-  return <>{/* <Navbar /> */}</>;
+  return (
+    <>
+      <Main />
+    </>
+  );
 }
 
